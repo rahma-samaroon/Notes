b64f44478036bba4eac26d238ca5fa53565b6565
Rahma Ahmed <rahmed.sammaron@gmail.com>
2022-10-23 15:35:30 +0200

    Add project files.

---
+++ Notes/Controllers/NotesController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notes.Data;
+using Notes.Models;
+
+namespace Notes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly NotesContext _context;
+
+        public NotesController(NotesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Notes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Note>>> GetNote()
+        {
+            List<Note> notes = await _context.Notes.ToListAsync();
+            return Ok(notes);
+        }
+
+        // GET: api/Notes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> GetNote(int id)
+        {
+            var note = await _context.Notes.FindAsync(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(note);
+        }
+
+        // PUT: api/Notes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNote(int id, Note note)
+        {
+            if (id != note.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Notes.Attach(note);
+
+            if (note.Text != null)
+            {
+                _context.Entry(note).Property(x => x.Text).IsModified = true;
+            }
+
+            if (note.Name != null)
+            {
+                _context.Entry(note).Property(x => x.Name).IsModified = true;
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Notes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Note>> PostNote(Note note)
+        {
+            note.CreatedDate = DateTime.UtcNow;
+            _context.Notes.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNote", new { id = note.Id }, note);
+        }
+
+        // DELETE: api/Notes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNote(int id)
+        {
+            var note = await _context.Notes.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            _context.Notes.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NoteExists(int id)
+        {
+            return _context.Notes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Notes/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
